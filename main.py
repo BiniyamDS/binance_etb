3c7017db347b45080ca8daeabaab6f0a351a8aa2
BiniyamDS <biniyam.yr@gmail.com>
2024-08-10 20:23:02 +0300

    added proxy to circumvent restrictions

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 import requests
 import pandas as pd
-import time
 from datetime import datetime
 from sqlalchemy import create_engine
 import os
 from dotenv import load_dotenv
+from fp.fp import FreeProxy
 
 # Load environment variables from a .env file
 load_dotenv()
 
 # Database URL
 DATABASE_URL = os.getenv('DB_URL')
-print(f'The database URL is: {DATABASE_URL}')
+try:
+    PROXY = FreeProxy().get()
+    print(f'Using proxy: {PROXY}')
+except:
+    print('Error getting proxy')
+    exit(1)
+
+proxies = {
+    "http": PROXY,
+    "https": PROXY,
+}
 
 # Create SQLAlchemy engine
 try:
@@ -39,20 +49,23 @@ def fetch_binance_p2p_data(asset='USDT', trade_type='BUY', fiat='USD', page=1):
 
     try:
         print(f"Sending request to {url} with payload: {payload} and headers: {headers}")
-        response = requests.post(url, json=payload, headers=headers)
+        response = requests.post(url, json=payload, headers=headers, proxies=proxies, verify=False)
         response.raise_for_status()  # Raise an error for bad responses
         print(f"Response Status Code: {response.status_code}")
         print(f"Response Headers: {response.headers}")
         return response.json()
     except requests.exceptions.HTTPError as errh:
         print(f"HTTP Error: {errh}")
+        exit(1)
     except requests.exceptions.ConnectionError as errc:
         print(f"Error Connecting: {errc}")
+        exit(1)
     except requests.exceptions.Timeout as errt:
         print(f"Timeout Error: {errt}")
+        exit(1)
     except requests.exceptions.RequestException as err:
         print(f"Something went wrong: {err}")
-    return None
+        exit(1)
 
 def store_p2p_data_to_db(buy_data, sell_data):
     records = []
@@ -97,6 +110,7 @@ def store_p2p_data_to_db(buy_data, sell_data):
         try:
             df.to_sql('binance_p2p_data', engine, if_exists='append', index=False)
             print('Data stored successfully in the database.')
+            print(f"Data stored at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
         except Exception as e:
             print(f'Error adding to DB: {e}')
 
@@ -111,7 +125,3 @@ if __name__ == "__main__":
 
     # Store the data in the database
     store_p2p_data_to_db(buy_data, sell_data)
-    
-    print(f"Data stored at {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
-    
-    # Sleep for a specified interval (e.g., 1 hour)
